520b6e60ace122e6b35d2dbaf5b892db6ef264d5
Armin Varshokar <arminvarshokar@gmail.com>
2025-05-03 01:37:08 -0400

    Enable debugging (to logs.txt)

--- src/logging.rs
+++ src/logging.rs
@@ -12,7 +12,8 @@ lazy_static::lazy_static! {
 pub fn init() -> Result<()> {
     let directory = config::get_data_dir();
     std::fs::create_dir_all(directory.clone())?;
-    let log_path = directory.join(LOG_FILE.clone());
+    // let log_path = directory.join(LOG_FILE.clone());
+    let log_path = "/Users/armin/GitHub/Rust/daily-dashboard/logs.txt";
     let log_file = std::fs::File::create(log_path)?;
     let env_filter = EnvFilter::builder().with_default_directive(tracing::Level::INFO.into());
     // If the `RUST_LOG` environment variable is set, use that as the default, otherwise use the
